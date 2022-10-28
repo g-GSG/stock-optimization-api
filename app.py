94cabff8df84b3a6994bad749b9fedd027912d68
Giulia Chiucchi <giuliachiucchi@ioasys.com.br>
2022-10-28 16:24:36 -0300

    fixes no backend

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask
+# ---------- import libs ---------
+from flask import Flask, request
+
+# ----------- routes and applications --------
 app = Flask(__name__)
-@app.route('/')
+
+# Root page
+@app.route("/")
 def hello_world():
-    return 'Hello world!'
+    return "Python PL optimization!"
+
+@app.route("/optimization", methods=["POST"])
+def optimization():
+    if request.method == "POST":
+        return request.json
